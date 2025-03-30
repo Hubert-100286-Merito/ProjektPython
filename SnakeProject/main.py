366abdea18ee88b35651aaaf25c60d0b094f7a68
Hubert Przybylski - wrx93068 - Merito <wrx93068@student.wroclaw.merito.pl>
2025-03-30 11:06:51 +0200

    Update main.py
    Co-Authored-By: Jakub100351 <168918578+Jakub100351@users.noreply.github.com>

---
+++ SnakeProject/main.py
@@ -0,0 +1,21 @@
+import pygame 
+import sys
+import random
+
+pygame.init()
+
+SW, SH = 800, 800
+
+
+
+screen = pygame.display.set_mode((SW, SH))
+pygame.display.set_caption("Snake Test nr.1")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(10)
