3d21eaf84b3a730594d4f7cddb1b8f023bbfbed2
Hubert Przybylski - wrx93068 - Merito <wrx93068@student.wroclaw.merito.pl>
2025-03-30 11:17:25 +0200

    Tworzenie Siatki

--- SnakeProject/main.py
+++ SnakeProject/main.py
@@ -6,11 +6,20 @@ pygame.init()
 
 SW, SH = 800, 800
 
-Font = pygame.font.Font("font.ttf", 100)
+Rozmiar_Siatki = 50
+Font = pygame.font.Font("font.ttf", Rozmiar_Siatki*2)
 
 screen = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("Snake Test nr.1")
 clock = pygame.time.Clock()
+
+def Siatka():
+    for x in range(0, SW, Rozmiar_Siatki):
+        for y in range(0, SH, Rozmiar_Siatki):
+            rect = pygame.Rect(x, y, Rozmiar_Siatki, Rozmiar_Siatki)
+            pygame.draw.rect(screen, (157, 157, 157), rect, 1)
+
+Siatka()
 
 while True:
     for event in pygame.event.get():
